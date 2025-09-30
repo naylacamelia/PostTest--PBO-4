7bbce71f83b02e6a3f2b1cec675f48be566f17af
naylacamelia <naylacame@gmail.com>
2025-09-30 20:51:48 +0800

    Delete java directory

--- java/Model/Prioritas.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public interface Prioritas {
-    String tentukanPrioritas();
-
-    default void tampilkanPrioritas() {
-        System.out.println("Prioritas   : " + tentukanPrioritas());
-    }
-}
